3553618f9a8890d50e81988b5257722e01a01acc
兰焱 <lanyan.null@bytedance.com>
2021-06-27 22:59:42 +0800

    add tag

--- hello.go
+++ hello.go
@@ -4,10 +4,10 @@ import "fmt"
 
 // Hello says hello.
 func Hello() {
-	fmt.Println("Hello go mod!")
+	fmt.Println("Hello go mod for TAG!")
 }
 
 // Bye says bye.
 func Bye() {
-	fmt.Println("Bye go mod!")
+	fmt.Println("Bye go mod for TAG!")
 }
